e24b25577d858c5ba3d90fbc93c146e211f53a47
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2022-02-17 20:18:27 -0500

    wip

--- src/utils/ark.tsx
+++ src/utils/ark.tsx
@@ -44,15 +44,8 @@ import * as Bip44 from "./bip44";
             Apdu.Flag.INS_SIGN_MESSAGE,
             Apdu.Flag.P1_SINGLE,
             Apdu.Flag.P2_SCHNORR_LEG,
-            Buffer.from(
-               Buffer.concat(
-                  [
-                     Bip44.Path.fromString(path).toBytes(),
-                     Buffer.from(message)
-                  ]
-               )
-            ),
-        ).getInstruction().flat()toString().split('/(e0)/');
+            Buffer.from(Buffer.concat([Bip44.Path.fromString(path).toBytes(), Buffer.from(message)])),
+        ).getInstruction().toString().split('/(e0)/');
     }
 
     export function getTransactionInstruction(path: string, payload: string): string {
